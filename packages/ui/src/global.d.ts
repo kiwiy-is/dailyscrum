34da7acd0d9bebe1c3e3b0d3f3b4d0c0885c4f1f
Hyo Kwon <hyo.kwon@outlook.com>
2024-02-10 02:22:51 +0900

    Setup ui components package #3
    Resolve #3

---
+++ packages/ui/src/global.d.ts
@@ -0,0 +1,21 @@
+import type * as CLSX from 'clsx';
+
+declare module 'class-variance-authority/types' {
+  export type ClassPropKey = 'class' | 'className';
+  export type ClassValue = CLSX.ClassValue;
+  export type ClassProp =
+    | {
+        class: ClassValue;
+        className?: never;
+      }
+    | {
+        class?: never;
+        className: ClassValue;
+      }
+    | {
+        class?: never;
+        className?: never;
+      };
+  export type OmitUndefined<T> = T extends undefined ? never : T;
+  export type StringToBoolean<T> = T extends 'true' | 'false' ? boolean : T;
+}
