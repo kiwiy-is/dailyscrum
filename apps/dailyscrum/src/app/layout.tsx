c26a4d54163988358ec9b941d16fd7172fb696bf
Hyuk Kwon <hyuk.kwon.epic@outlook.com>
2024-02-15 00:57:26 +0900

    Build basic layout setup #7
    Resolve #7

--- apps/dailyscrum/src/app/layout.tsx
+++ apps/dailyscrum/src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <body className={`${inter.className} antialiased`}>{children}</body> */}
       <body className={cn(inter.className, "antialiased")}>{children}</body>
     </html>
   );
