692fbe4c9aa54cc025ce43ffda350164913e036d
Hyuk Kwon <hyuk.kwon.epic@outlook.com>
2024-02-14 22:18:26 +0900

    Setup and deploy website & dailyscrum apps #5 (#6)
    * Setup and deploy website & dailyscrum apps
    #5
    
    Resolve #5
    
    * Setup and deploy website & dailyscrum apps #5

--- apps/dailyscrum/next.config.mjs
+++ apps/dailyscrum/next.config.mjs
@@ -1,4 +1,28 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix: isProd ? "https://dailyscrum.fly.dev" : undefined,
+  async headers() {
+    return [
+      {
+        source: "/:path*", // /_next/static/media/c9a5bc6a7c948fb0-s.p.woff2'
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
---
+++ apps/website/src/app/samples/[[...all]]/route.ts
@@ -0,0 +1,6 @@
+export async function GET(request: Request) {
+  return new Response(null, {
+    status: 204,
+    headers: { "fly-replay": "app=dailyscrum" },
+  });
+}
