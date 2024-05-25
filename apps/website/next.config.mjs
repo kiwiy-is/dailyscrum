e67b28a39704d5b5a45efc1ef1aff7ea2a7a8cfb
Hyuk Kwon <hyuk.kwon.epic@outlook.com>
2024-05-25 10:27:57 +0900

    Setup CI/CD with github actions #121
    Resolve #121

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -1,4 +1,15 @@
+const proxyUrl = process.env.PROXY_URL || "";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/app/:match*",
+        destination: `${proxyUrl}/app/:match*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- apps/website/src/app/app/[[...all]]/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET(request: Request) {
-  return new Response(null, {
-    status: 204,
-    headers: { "fly-replay": "app=dailyscrum-core" },
-  });
-}
