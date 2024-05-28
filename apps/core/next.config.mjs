e15c8906f0c0111259c9ec3e534564758c36f735
Hyuk Kwon <hyuk.kwon.epic@outlook.com>
2024-05-28 10:03:03 +0900

    Properly pass env secrets on build time for core app #131
    Resolve #131

--- apps/core/next.config.mjs
+++ apps/core/next.config.mjs
@@ -3,12 +3,6 @@ const assetPrefix = process.env.ASSET_PREFIX || "";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   assetPrefix,
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
-    ASSET_PREFIX: process.env.ASSET_PREFIX,
-  },
   async headers() {
     return [
       {
