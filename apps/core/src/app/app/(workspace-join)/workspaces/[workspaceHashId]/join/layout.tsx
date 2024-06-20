ea4775f29cec91ae979bba30265227652d395dc7
Hyuk Kwon <hyuk.kwon.epic@outlook.com>
2024-06-20 22:21:49 +0900

    Join workspace layout is broken #160
    Resolve #160

--- apps/core/src/app/app/(workspace-join)/workspaces/[workspaceHashId]/join/layout.tsx
+++ apps/core/src/app/app/(workspace-join)/workspaces/[workspaceHashId]/join/layout.tsx
@@ -1,17 +1,12 @@
+import CenteredContentLayout from "@/components/centered-content-layout";
 import React from "react";
-import { KiwiyIsSymbol } from "ui/kiwiy-is-symbol";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center space-y-8 px-4">
-      <KiwiyIsSymbol width={32} height={32} />
-      <div className="w-full sm:!w-[440px]">{children}</div>
-    </div>
-  );
+  return <CenteredContentLayout>{children}</CenteredContentLayout>;
 };
 
 export default Layout;
